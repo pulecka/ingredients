5a88024603f8efd89b10b3681336a2183310c548
Martin Obert <me@pulecka.com>
2015-09-27 23:37:32 +0200

    added dynamic validation dependent on other field's value

--- src/app/modules/iValid/validators/common.js
+++ src/app/modules/iValid/validators/common.js
@@ -18,6 +18,8 @@
     iValidProvider.newValidator('maxLength', maxLength, 'This is too long!');
     iValidProvider.newValidator('minLength', minLength, 'Too short!');
     iValidProvider.newValidator('required', required, 'This field is required!');
+    iValidProvider.newValidator('match', match, 'This should match the other field!', true);
+    iValidProvider.newValidator('unique', unique, 'This should be different from the other field!', true);
 
     function maxLength(value, max) {
       if (value) {
@@ -40,6 +42,24 @@
         return value.toString().length > 0;
       } else {
         return false;
+      }
+    }
+
+    function match(value, model, scope) {
+      var matchingValue = model(scope)
+      if (value && matchingValue) {
+        return value === matchingValue;
+      } else {
+        return true;
+      }
+    }
+
+    function unique(value, model, scope) {
+      var uniqueValue = model(scope);
+      if (value && uniqueValue) {
+        return value !== uniqueValue;
+      } else {
+        return true;
       }
     }
   }
